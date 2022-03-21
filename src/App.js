9aae2d6ca99f0f8125d38672a6fad15649c24f62
ullash4 <programmerullash@gmail.com>
2022-03-21 09:09:38 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Consmic from './components/Cosmic/Consmic';
 
 function App() {
   return (
     <div className="App">
-      
+      <Consmic />
     </div>
   );
 }
---
+++ src/components/Cosmic/Consmic.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Consmic = () => {
+    return (
+        <div>
+            <h1>This is Cosmic World</h1>
+        </div>
+    );
+};
+
+export default Consmic;
